3fe01f693f281de3401190fea5015e83c9d02500
Alexander Gomez <31171324+agomezssp@users.noreply.github.com>
2024-10-31 19:26:23 +0000

    data

--- streamline_app.py
+++ streamline_app.py
@@ -8,30 +8,34 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.DataFrame(np.random.randint(0,100, size=(100,4)))
+# df = pd.DataFrame(np.random.randint(0,100, size=(100,4)))
+# st.line_chart(df)
 
-st.line_chart(df)
-
-
-
-aws_session = Session(
-    aws_access_key_id = st.secrets.default.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=st.secrets.default.AWS_SECRET_ACCESS_KEY,
-    region_name=st.secrets.default.AWS_DEFAULT_REGION
-)
 
 def load_data(): 
     table = DeltaTable(
-        table_uri="s3://",
+        table_uri="s3://cuponlub-ods-us-west-2-bucket-891377106068-dev/02_staging/tables/contacto/",
         storage_options = { 
             "AWS_ACCESS_KEY_ID": st.secrets.default.AWS_ACCESS_KEY_ID,
             "AWS_SECRET_ACCESS_KEY": st.secrets.default.AWS_SECRET_ACCESS_KEY,
             "REGION" :st.secrets.default.AWS_DEFAULT_REGION
         }
     )
-    versions = [str(item.get("readVersion", -1) + 1) + " - " + datetime.fromtimestamp(item.get()) for item in table]
-    versions[-1] = str(table.version()) + " - Lastest"
+
+    # versions = [str(item.get("readVersion", -1) + 1) + " - " + datetime.fromtimestamp(item.get()) for item in table.version()]
+    # versions[-1] = str(table.version()) + " - Lastest"
+    versions = []
     return table, versions
 
 
+
+st.title("AWS Delta Lake Data")
+
+delta_table_data, versions = load_data()
+
+with st.sidebar:
+      version = st.selectbox("Seleccione versión:", versions, len(versions) -1)
+
+df = delta_table_data.to_pandas()
+st.table(data= df)
     
