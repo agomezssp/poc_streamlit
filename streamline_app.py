77904ec007571a35c327a1e96b92ce6866672255
Alexander Gomez <31171324+agomezssp@users.noreply.github.com>
2024-10-31 15:05:13 +0000

    requirements

--- streamline_app.py
+++ streamline_app.py
@@ -12,10 +12,26 @@ df = pd.DataFrame(np.random.randint(0,100, size=(100,4)))
 
 st.line_chart(df)
 
+
+
 aws_session = Session(
     aws_access_key_id = st.secrets.default.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=st.secrets.default.AWS_SECRET_ACCESS_KEY,
-    region_name="us-west-2"
+    region_name=st.secrets.default.AWS_DEFAULT_REGION
 )
 
-                  
+def load_data(): 
+    table = DeltaTable(
+        table_uri="s3://",
+        storage_options = { 
+            "AWS_ACCESS_KEY_ID": st.secrets.default.AWS_ACCESS_KEY_ID,
+            "AWS_SECRET_ACCESS_KEY": st.secrets.default.AWS_SECRET_ACCESS_KEY,
+            "REGION" :st.secrets.default.AWS_DEFAULT_REGION
+        }
+    )
+    versions = [str(item.get("readVersion", -1) + 1) + " - " + datetime.fromtimestamp(item.get()) for item in table]
+    versions[-1] = str(table.version()) + " - Lastest"
+    return table, versions
+
+
+    
